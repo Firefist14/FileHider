87bb04a207e4ca4d863abd116f55c320307572f0
jaiKandpal <75561056+Firefist14@users.noreply.github.com>
2024-12-26 19:25:26 +0530

    Update MyConnection.java

--- FileHider/src/main/java/db/MyConnection.java
+++ FileHider/src/main/java/db/MyConnection.java
@@ -8,7 +8,7 @@ public class MyConnection {
     public static Connection connection;
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/FileHider?useSSL=false","root", "watch@dogs*24");
+        connection = DriverManager.getConnection("sqllink","username", "password");
         System.out.println("Connection Established");
         return connection;
     }
